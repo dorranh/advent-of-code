be4a69807bb1ce33bd03f86625a98337e5f7d84a
Dorran Howell <dorran.howell@gmail.com>
2023-12-02 16:38:41 +0100

    Add scaffolding for 2023 project and day one

---
+++ 2023/rust/src/days/one.rs
@@ -0,0 +1,5 @@
+pub mod one {
+    pub fn solution() {
+        println!("This solution is a WIP")
+    }
+}
--- 2023/rust/src/main.rs
+++ 2023/rust/src/main.rs
@@ -1,3 +1,24 @@
+use aoc_2023::days::one::one;
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Number of the day to run
+    #[arg(short, long)]
+    day: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    if args.day == 1 {
+        one::solution();
+    } else {
+        println!(
+            "Unknown day {}. Make sure the supplied value ranges between 1-25.",
+            args.day
+        );
+    }
 }
