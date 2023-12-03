33ae8a89244e42195ee6e2f5136cef4dadd7e29d
Dorran Howell <dorran.howell@gmail.com>
2023-12-03 12:23:10 +0100

    Add solution: day 1, part 1

--- 2023/rust/src/days/one.rs
+++ 2023/rust/src/days/one.rs
@@ -1,5 +1,26 @@
 pub mod one {
-    pub fn solution() {
-        println!("This solution is a WIP")
+
+    /// Returns the calibration code from the input string, unsafely
+    /// panicking if the input does not contain a digit.
+    fn digits(puzzle_line: &String) -> u32 {
+        let filtered_chars: String = puzzle_line
+            .chars()
+            .filter(|&c| c.is_ascii_digit())
+            .collect();
+        let first: char = filtered_chars
+            .chars()
+            .next()
+            .unwrap_or_else(|| panic!("Failed to find a digit."));
+        let last: char = filtered_chars
+            .chars()
+            .last()
+            .unwrap_or_else(|| panic!("Failed to find a digit."));
+        let value: u32 = format!("{}{}", first, last).parse::<u32>().unwrap();
+        value
+    }
+
+    pub fn solution(lines: Vec<String>) {
+        let result: u32 = lines.iter().map(|l| digits(l)).sum();
+        println!("Decoded the calibration code. Result: {}", result);
     }
 }
--- 2023/rust/src/main.rs
+++ 2023/rust/src/main.rs
@@ -1,5 +1,7 @@
 use aoc_2023::days::one::one;
 use clap::Parser;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -10,15 +12,20 @@ struct Args {
     day: u8,
 }
 
-fn main() {
+fn main() -> io::Result<()> {
     let args = Args::parse();
 
     if args.day == 1 {
-        one::solution();
+        // FIXME: Make the input path a CLI arg
+        let file = File::open("data/puzzle-input-1.txt")?;
+        let reader = BufReader::new(file);
+        let lines: Vec<String> = reader.lines().filter_map(|line| line.ok()).collect();
+        one::solution(lines);
     } else {
         println!(
             "Unknown day {}. Make sure the supplied value ranges between 1-25.",
             args.day
         );
     }
+    Ok(())
 }
